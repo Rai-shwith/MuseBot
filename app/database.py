5fd16f6d7a5c2db48ec85f74e2bb62e13b6d296e
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2024-08-31 16:50:11 +0530

    Defined function to insert into the dummuDB

--- app/database.py
+++ app/database.py
@@ -1,7 +1,18 @@
-import os 
 import json
+from typing import Union
+from pydantic import BaseModel
 
 def get_db():
-    with open('dummy_db/db.json', 'r') as f:
-        data = json.load(f)
-    return data
+    with open('dummy_db/db.json','r') as d:
+        db = json.load(d)
+    return db
+
+def put_in_db(table_name:str,entity:Union[dict,BaseModel]):
+    # Convert the entity to a dictionary if it's a Pydantic model
+    if isinstance(entity, BaseModel):
+        entity = entity.model_dump()
+    db = get_db()
+    required_table = db[table_name]
+    required_table.append(entity)
+    with open('dummy_db/db.json',"w") as d:
+        json.dump(db,d,indent=4)
